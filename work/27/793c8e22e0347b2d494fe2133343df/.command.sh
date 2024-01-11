c4bb8c01058b5a21324e247791ddc1d273842078
itrujnara <itrujnara@wp.pl>
2024-01-11 10:15:43 +0100

    Reorganized the directory

--- work/27/793c8e22e0347b2d494fe2133343df/.command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -euo pipefail
-check_samplesheet.py \
-    samplesheet.csv \
-    samplesheet.valid.csv
-
-cat <<-END_VERSIONS > versions.yml
-"NFCORE_PROTEINFOLD:ALPHAFOLD2:INPUT_CHECK:SAMPLESHEET_CHECK":
-    python: $(python --version | sed 's/Python //g')
-END_VERSIONS
